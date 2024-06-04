e38deb29b8e70d169c281aeff0efe3aa61ff3c8a
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-06-04 10:00:33 +0300

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
+const imageCard = document.querySelector('.js-main-person');
 
+imageCard.addEventListener('click', event => {
+  const rect = imageCard.getBoundingClientRect();
+  const offsetX = event.clientX - rect.left;
+  const offsetY = event.clientY - rect.top;
+  const centerX = rect.width / 2;
+  const centerY = rect.height / 2;
+
+  const rotateX = ((offsetY - centerY) / centerY) * 15;
+  const rotateY = ((centerX - offsetX) / centerX) * 15;
+
+  imageCard.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+
+  setTimeout(resetImageStyle, 100);
+});
+
+function resetImageStyle() {
+  imageCard.style.transform = 'rotateX(0deg) rotateY(0deg)';
+}
